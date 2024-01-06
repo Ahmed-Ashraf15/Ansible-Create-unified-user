ca4953c3cf33b0f1bdd13450ff4b1d41a2bfe807
Ahmed Ashraf <113308790+Ahmed-Ashraf15@users.noreply.github.com>
2024-01-06 13:35:02 +0200

    Add files via upload

---
+++ ansible.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# generate public key from private one
+
+echo -n "enter the private key path: "
+read key_path
+
+if [ -e "./public_key.pem" ]; then
+    echo "File 'public_key.pem' already exists. Skipping creation."
+else
+    ssh-keygen -f ./$key_path.pem -y > ./public_key.pem
+    echo "File 'public_key.pem' created."
+fi
+
+
+# run ansible playbook with specific root user input from user
+
+echo -n 'enter the path of file: '
+read file_name
+
+echo -n 'enter the root user '
+read remote_user  
+
+ansible-playbook ./$file_name.yaml -u $remote_user
